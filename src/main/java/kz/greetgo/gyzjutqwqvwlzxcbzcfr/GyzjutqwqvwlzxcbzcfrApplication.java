05a0c6be45d2296b793931ab75674791eed7e16a
Alikhan Kaliyev <alikhanfrontend22@gmail.com>
2023-10-23 18:51:50 +0600

    initialized project

---
+++ src/main/java/kz/greetgo/gyzjutqwqvwlzxcbzcfr/GyzjutqwqvwlzxcbzcfrApplication.java
@@ -0,0 +1,13 @@
+package kz.greetgo.gyzjutqwqvwlzxcbzcfr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GyzjutqwqvwlzxcbzcfrApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GyzjutqwqvwlzxcbzcfrApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kz/greetgo/gyzjutqwqvwlzxcbzcfr/GyzjutqwqvwlzxcbzcfrApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.greetgo.gyzjutqwqvwlzxcbzcfr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GyzjutqwqvwlzxcbzcfrApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
